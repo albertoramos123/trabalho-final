5a3abd9c114367698af97b60df74213861c103e5
alberto ramos <162235872+albertoramos123@users.noreply.github.com>
2024-05-24 01:25:07 +0000

    Descrição das alterações feitas

---
+++ script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão pela classe
+var botaoEntrar = document.querySelector('.login-button');
+
+// Adiciona um ouvinte de evento para detectar o clique no botão
+botaoEntrar.addEventListener('click', function() {
+    // Aqui você pode colocar o código do alerta que deseja exibir
+    alert("senha incorreta !!!");
+});
